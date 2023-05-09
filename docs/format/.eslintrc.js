4e56a4b1430d486354d0827f49401a1e52270057
ShinMini <77220824+ShinMini@users.noreply.github.com>
2023-05-09 10:44:11 +0900

    docs: create eslintrc default file

---
+++ docs/format/.eslintrc.js
@@ -0,0 +1,13 @@
+// .eslintrc.js
+
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    // 여기에 추가 규칙을 정의할 수 있습니다.
+  },
+};
